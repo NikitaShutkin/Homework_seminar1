a87af5796c743f0c6bfc6dc3eb694280b0c45692
Nikita <nikita.serg.utkin@gmail.com>
2022-12-01 17:06:32 +0900

    Рабочий вариант

---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое целое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1>num2)
+{
+    Console.WriteLine($"Большее число {num1}");
+    Console.WriteLine($"Меньшее число {num2}");
+}
+else if (num2>num1)
+{
+    Console.WriteLine($"Большее число {num2}");
+    Console.WriteLine($"Меньшее число {num1}");
+}
+else
+{
+    Console.WriteLine("Введенные числа одинаковы");
+}
